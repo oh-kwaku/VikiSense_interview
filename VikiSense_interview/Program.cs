5dbc837ff3a6fe9132b0126fc9f8b69879e57f11
kwakuemma37 <kwakuemma37@gmail.com>
2024-03-14 16:13:51 +0000

    fixes

--- VikiSense_interview/Program.cs
+++ VikiSense_interview/Program.cs
@@ -1,7 +1,4 @@
-using Microsoft.AspNetCore.ResponseCompression;
 using VikiSense_interview.Extension_Methods;
-using VikiSense_interview.Middlewares;
-using VikiSense_interview.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddResponseCompression(options =>
-{
-    options.Providers.Add<CustomCompressionProvider>();
-    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json", "application/xml" });
 
-});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. 
